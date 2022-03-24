c5492cb4dab5cc619b3df4e329f7ef44a968c354
AJ <39858427+Pp-4@users.noreply.github.com>
2022-03-24 16:50:18 +0100

    zadania 1 do 5 java

---
+++ untitled1/src/Main.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+
+        //zad1();
+        //zad2();
+        //zad3();
+        //zad4();
+        zad5();
+    }
+    public static void zad1(){
+        System.out.println("Podaj ciąg znaków");
+        Scanner scan = new Scanner(System.in);
+        String input = scan.nextLine();
+        StringBuilder chain = new StringBuilder(input);
+        char a = chain.charAt(chain.length()-1);
+        int count = 0;
+        for(int i=0;i<chain.length();i++){
+            if(chain.charAt(i)==a)count++;
+        }
+        System.out.println("Liczba wystąpień : " + count);
+    }
+    public static void zad2(){
+        System.out.println("Podaj ciąg znaków");
+        Scanner scan = new Scanner(System.in);
+        String input = scan.nextLine();
+        StringBuilder chain = new StringBuilder(input);
+        chain.reverse();
+        System.out.println("Odwrócony tekst : " + chain);
+    }
+    public static void zad3(){
+        System.out.println("Podaj ciąg znaków");
+        Scanner scan = new Scanner(System.in);
+        String input = scan.nextLine();
+        StringBuilder chain = new StringBuilder(input);
+        chain.reverse();
+        boolean test = input.contentEquals(chain);
+        if(test)System.out.println("Podany ciąg jest palindromem.");
+        else System.out.println("Podany ciąg nie jest palindromem.");
+    }
+    public static void zad4(){
+        System.out.println("Podaj ciąg znaków");
+        Scanner scan = new Scanner(System.in);
+        StringBuilder chain = new StringBuilder(scan.nextLine());
+        int suma = 0;
+        for(int i = 0; i < chain.length(); i++){
+            char temp = chain.charAt(i);
+            if(Character.isDigit(temp))suma += temp - 48;
+        }
+        System.out.println("Suma cyfr znalezionych w ciągu : " + suma);
+    }
+    public static void zad5(){
+        System.out.println("Podaj ciąg znaków");
+        Scanner scan = new Scanner(System.in);
+        StringBuilder chain = new StringBuilder(scan.nextLine());
+        int LeftBracket = 0;
+        int RightBracket = 0;
+        for(int i = 0; i < chain.length(); i++){
+            char temp = chain.charAt(i);
+            if(temp == '(')LeftBracket++;
+            if(temp == ')')RightBracket++;
+        }
+        String msg = (LeftBracket == RightBracket)?"OK":"Nie OK";
+        System.out.println(msg);
+    }
+}
