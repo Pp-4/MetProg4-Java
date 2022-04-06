c00ec409c982be16b8675fd6c236317d193e3037
AJ-pP4 <39858427+Pp-4@users.noreply.github.com>
2022-04-07 01:40:03 +0200

    działa zadanie z godzinami

--- src/App.java
+++ src/App.java
@@ -1,97 +1,132 @@
 import java.util.Scanner;
 import java.util.Arrays;
-public class App {
-    public static void main(String[] args) throws Exception {
-            //zad1();
-            //zad2();
-            //zad3();
-            //zad4();
-            //zad5();
-            zad_układanie_godziny();
-        }
-        public static void zad1(){
-            System.out.println("Podaj ciąg znaków");
-            Scanner scan = new Scanner(System.in);
-            String input = scan.nextLine();
-            StringBuilder chain = new StringBuilder(input);
-            char a = chain.charAt(chain.length()-1);
-            int count = 0;
-            for(int i=0;i<chain.length();i++){
-                if(chain.charAt(i)==a)count++;
-            }
-            System.out.println("Liczba wystąpień : " + count);
-        }
-        public static void zad2(){
-            System.out.println("Podaj ciąg znaków");
-            Scanner scan = new Scanner(System.in);
-            String input = scan.nextLine();
-            StringBuilder chain = new StringBuilder(input);
-            chain.reverse();
-            System.out.println("Odwrócony tekst : " + chain);
-        }
-        public static void zad3(){
-            System.out.println("Podaj ciąg znaków");
-            Scanner scan = new Scanner(System.in);
-            String input = scan.nextLine();
-            StringBuilder chain = new StringBuilder(input);
-            chain.reverse();
-            boolean test = input.contentEquals(chain);
-            if(test)System.out.println("Podany ciąg jest palindromem.");
-            else System.out.println("Podany ciąg nie jest palindromem.");
-        }
-        public static void zad4(){
-            System.out.println("Podaj ciąg znaków");
-            Scanner scan = new Scanner(System.in);
-            StringBuilder chain = new StringBuilder(scan.nextLine());
-            int suma = 0;
-            for(int i = 0; i < chain.length(); i++){
-                char temp = chain.charAt(i);
-                if(Character.isDigit(temp))suma += temp - 48;
-            }
-            System.out.println("Suma cyfr znalezionych w ciągu : " + suma);
-        }
-        public static void zad5(){
-            System.out.println("Podaj ciąg znaków");
-            Scanner scan = new Scanner(System.in);
-            StringBuilder chain = new StringBuilder(scan.nextLine());
-            int LeftBracket = 0, RightBracket = 0;
-            for(int i = 0; i < chain.length(); i++){
-                char temp = chain.charAt(i);
-                if(temp == '(')LeftBracket++;
-                if(temp == ')')RightBracket++;
-            }
-            String msg = (LeftBracket == RightBracket)?"OK":"Nie OK";
-            System.out.println(msg);
-        }
-        public static void zad_układanie_godziny(){
-            System.out.println("Podaj ciąg czterech cyfr");
-            Scanner scan = new Scanner(System.in);
-            StringBuilder chain = new StringBuilder(scan.nextLine());
-            if(chain.length()!=4) System.out.println("Nieprawidłowa długość ciągu!");
-            else{
-                int length = chain.length()*chain.length();
-                int[][] tablicaGodzin = new int[length][4];
-                for(int i = 0;i<chain.length();i++) {
-                    for(int j = 0;j<chain.length();j++){
-                        int temp = (chain.charAt(i)-48)*10+(chain.charAt(j)-48);
-                        tablicaGodzin[i*j][0] = temp > 23 ? 0 : chain.charAt(i)-48;
-                        tablicaGodzin[i*j][1] = temp > 23 ? 0 : chain.charAt(j)-48;
-                        String h = "1234";
-                        h.replace(Integer.toString(i-48),"");
-                        h.replace(Integer.toString(j-48),"");
-                        tablicaGodzin[i*j][2] = temp > 23 ? 0 : chain.charAt(h.charAt(0));
-                        tablicaGodzin[i*j][3] = temp > 23 ? 0 : chain.charAt(h.charAt(1));
-                    }
-                }
-                int[] wyniki = new int[length];
-                for(int i = 0;i < length; i++){
-                    int temp = tablicaGodzin[i][2]>=tablicaGodzin[i][3]?(tablicaGodzin[i][2]-48)*10+(tablicaGodzin[i][3]-48):(tablicaGodzin[i][3]-48)*10+(tablicaGodzin[i][2]-48);
-                    temp = temp>60?0:temp;
-                    wyniki[i] = (tablicaGodzin[i][0]-48)*10+tablicaGodzin[i][1]-48+temp;
-                }
-                Arrays.sort(wyniki);
-                System.out.println(wyniki[wyniki.length-1]);
-        }
 
+public class App {
+    public static void main(String[] args) {
+        // zad1();
+        // zad2();
+        // zad3();
+        // zad4();
+        // zad5();
+        permutacje();
+    }
+
+    public static void zad1() {
+        System.out.println("Podaj ciąg znaków");
+        Scanner scan = new Scanner(System.in);
+        String input = scan.nextLine();
+        StringBuilder chain = new StringBuilder(input);
+        scan.close();
+        char a = chain.charAt(chain.length() - 1);
+        int count = 0;
+        for (int i = 0; i < chain.length(); i++) {
+            if (chain.charAt(i) == a)
+                count++;
+        }
+        System.out.println("Liczba wystąpień : " + count);
+    }
+
+    public static void zad2() {
+        System.out.println("Podaj ciąg znaków");
+        Scanner scan = new Scanner(System.in);
+        String input = scan.nextLine();
+        StringBuilder chain = new StringBuilder(input);
+        scan.close();
+        chain.reverse();
+        System.out.println("Odwrócony tekst : " + chain);
+    }
+
+    public static void zad3() {
+        System.out.println("Podaj ciąg znaków");
+        Scanner scan = new Scanner(System.in);
+        String input = scan.nextLine();
+        StringBuilder chain = new StringBuilder(input);
+        scan.close();
+        chain.reverse();
+        boolean test = input.contentEquals(chain);
+        if (test)
+            System.out.println("Podany ciąg jest palindromem.");
+        else
+            System.out.println("Podany ciąg nie jest palindromem.");
+    }
+
+    public static void zad4() {
+        System.out.println("Podaj ciąg znaków");
+        Scanner scan = new Scanner(System.in);
+        StringBuilder chain = new StringBuilder(scan.nextLine());
+        scan.close();
+        int suma = 0;
+        for (int i = 0; i < chain.length(); i++) {
+            char temp = chain.charAt(i);
+            if (Character.isDigit(temp))
+                suma += temp - 48;
+        }
+        System.out.println("Suma cyfr znalezionych w ciągu : " + suma);
+    }
+
+    public static void zad5() {
+        System.out.println("Podaj ciąg znaków");
+        Scanner scan = new Scanner(System.in);
+        StringBuilder chain = new StringBuilder(scan.nextLine());
+        scan.close();
+        int LeftBracket = 0, RightBracket = 0;
+        for (int i = 0; i < chain.length(); i++) {
+            char temp = chain.charAt(i);
+            if (temp == '(')
+                LeftBracket++;
+            if (temp == ')')
+                RightBracket++;
+        }
+        String msg = (LeftBracket == RightBracket) ? "OK" : "Nie OK";
+        System.out.println(msg);
+    }
+
+    public static void permutacje() {
+        //funkcja wykonuje wszystkie permutacje tablicy,odrzuca nieprawidłowe godziny,sortuje pozostałe i zwraca największą godzinę
+        int n = 4;
+        System.out.println("Podaj ciąg czterech cyfr");
+        Scanner scan = new Scanner(System.in);
+        StringBuilder chain = new StringBuilder(scan.nextLine());
+        scan.close();
+        if (chain.length() != n)
+            System.out.println("Nieprawidłowa długość ciągu!");
+        else {
+
+            int[] tab = { (int) chain.charAt(0) - 48, (int) chain.charAt(1) - 48, (int) chain.charAt(2) - 48,
+                    (int) chain.charAt(3) - 48 };
+            String[] wynik = new String[24];// 4!=24
+            int count = 0;
+            int[] temp = new int[n];
+            for (int i = 0; i < n; i++) {
+                temp[i] = 0;
+            }
+            wynik[count] = tts(tab);
+            count++;
+            int i = 0;
+            while (i < n) {
+                if (temp[i] < i) {
+                    swap(tab, i % 2 == 0 ? 0 : temp[i], i);
+                    wynik[count] = tts(tab);
+                    count++;
+                    temp[i]++;
+                    i = 0;
+                } else
+                    temp[i++] = 0;
+            }
+            Arrays.sort(wynik);
+            System.out.print(wynik[wynik.length - 1]);
+        }
+    }
+
+    public static void swap(int[] arr, int a, int b) {// zamiana cyfr w tablicy
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+
+    public static String tts(int[] tab) {// sprawdza czy godzina jest poprawna
+        return (tab[0] * 10 + tab[1] > 23 || tab[2] * 10 + tab[3] > 59) ? "-1"
+                : Integer.toString(tab[0]) + Integer.toString(tab[1]) + ":" + Integer.toString(tab[2])
+                        + Integer.toString(tab[3]);
     }
 }
